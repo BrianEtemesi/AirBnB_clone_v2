028f097ccc88d91804e6fe8ed2248df6add7bf0c
BrianEtemesi <etemecci@gmail.com>
2023-03-27 18:59:32 +0000

    pycodestyle on test

--- tests/test_state.py
+++ tests/test_state.py
@@ -3,7 +3,10 @@
 import MySQLdb
 
 # connect to database
-db = MySQLdb.connect(host="HBNB_MYSQL_HOST", user="HBNB_MYSQL_USER", passwd="HBNB_MYSQL_PWD", db="HBNB_MYSQL_DB")
+db = MySQLdb.connect(host="HBNB_MYSQL_HOST",
+                     user="HBNB_MYSQL_USER",
+                     passwd="HBNB_MYSQL_PWD",
+                     db="HBNB_MYSQL_DB")
 cursor = db.cursor()
 
 # get current number of records
@@ -11,7 +14,10 @@ cursor.execute("SELECT COUNT(*) FROM states")
 num_records_before = cursor.fetchone()[0]
 
 # execute console command to insert new record
-console_command = "INSERT INTO states (column1, column2, column3) VALUES ('value1', 'value2', 'value3');"
+console_command = """
+INSERT INTO states (column1, column2, column3)
+VALUES ('value1', 'value2', 'value3');
+"""
 cursor.execute(console_command)
 db.commit()
 
