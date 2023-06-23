a07a204c52350b9bff1b95914d197700ae65b316
BrianEtemesi <etemecci@gmail.com>
2023-06-23 18:48:28 +0000

    change flow

--- 22-do_deploy_web_static.py
+++ 22-do_deploy_web_static.py
@@ -16,28 +16,22 @@ def do_deploy(archive_path):
     if not exists(archive_path):
         return False
 
-    compressed = archive_path.split("/")[-1]
-    uncompressed = compressed.split(".")[0]
-
     try:
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
 
         # Extract the archive to /data/web_static/releases/<archive filename without extension>
-        remote_folder = "/data/web_static/releases/{}/".format(uncompressed)
-        symbolic_conn = "/data/web_static/current"
+        archive_filename = os.path.basename(archive_path)
+        folder_name = '/data/web_static/releases/' + os.path.splitext(archive_filename)[0]
+        run('mkdir -p {}'.format(folder_name))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, folder_name))
+        run('rm /tmp/{}'.format(archive_filename))
 
-        run("mkdir -p {}".format(remote_folder))
-        run("tar -xvzf /tmp/{} -C {}".format(compressed, remote_folder))
-        run("rm /tmp/{}".format(compressed))
-        run("mv {}/web_static/* {}".format(remote_folder, remote_folder))
-        
         # Delete the symbolic link /data/web_static/current
-        run("rm -rf {}/web_static".format(remote_folder))
         run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link /data/web_static/current linked to the new version
-        run("ln -sf {} {}".format(remote_folder, symbolic_conn))
+        run('ln -s {} /data/web_static/current'.format(folder_name))
 
         print("New version deployed!")
         return True
